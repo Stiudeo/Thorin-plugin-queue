39e1dcd6ce8748543a790513ae0c246bbd011330
snupa <bunta.adrian@gmail.com>
2016-06-23 10:16:14 +0300

    updated the creation of a new queue

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = function(thorin, opt, pluginName) {
   const defaultOpt= {
     logger: pluginName || 'queue',
     store: null,          // the redis store to use for enqueue/dequeueing. If none is given, we work with an in-memory queue.
-    channel: 'tqueue',    // the default channel to enqueue to /dequeue from
+    channel: 'thorin.queue',    // the default channel to enqueue to /dequeue from
     batch: 10,             // the default number of items to dequeue at once.
     logFile: 'config/.queue',  // if specified, this will be the file that we're going to use to write queue logs.
     logPersist: 1000        // number of milliseconds between log persisting. This not to lose any enqueues
@@ -15,6 +15,7 @@ module.exports = function(thorin, opt, pluginName) {
   if(opt.logFile) {
     opt.logFile = path.normalize(path.isAbsolute(opt.logFile) ? opt.logFile : thorin.root + '/' + opt.logFile);
   }
+  function noop(){};
   const queueObj = initQueue(thorin, opt),
     logger = thorin.logger(opt.logger);
   /*
@@ -25,7 +26,9 @@ module.exports = function(thorin, opt, pluginName) {
     if(opt.logFile) {
       opt.logFile = path.normalize(path.isAbsolute(opt.logFile) ? opt.logFile : thorin.root + '/' + opt.logFile);
     }
-    return initQueue(thorin, opt);
+    let newQueue = initQueue(thorin, opt, true);
+    newQueue.run(noop);
+    return newQueue;
   }
   /*
   * Setup the queue plugin
